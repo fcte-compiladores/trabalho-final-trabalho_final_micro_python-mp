298302f30a3d17432afbc68b0257d8e4ad3b2495
Lucas Víctor Ferreira de Araújo <120221548+Lucas13032003@users.noreply.github.com>
2025-07-15 15:49:10 -0300

    main e exemplo

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+
+from lark import Lark
+from ast_builder import ASTBuilder
+from interpreter import Interpreter
+
+# Carrega a gramática
+with open("grammar.lark") as f:
+    grammar = f.read()
+
+parser = Lark(grammar, parser='lalr', transformer=ASTBuilder())
+interpreter = Interpreter()
+
+if len(sys.argv) < 2:
+    print("Uso: python main.py <arquivo_fonte>")
+    sys.exit(1)
+
+with open(sys.argv[1]) as f:
+    codigo = f.read()
+
+tree = parser.parse(codigo)
+for stmt in tree.children:
+    interpreter.exec(stmt)
