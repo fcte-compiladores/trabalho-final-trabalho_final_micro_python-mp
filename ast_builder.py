9283de9c180a074339d27b9b06aa5f352f534e51
Lucas Víctor Ferreira de Araújo <120221548+Lucas13032003@users.noreply.github.com>
2025-07-15 15:48:37 -0300

    ast

---
+++ ast_builder.py
@@ -0,0 +1,26 @@
+from lark import Transformer
+
+class ASTBuilder(Transformer):
+    def number(self, items):
+        return ('number', float(items[0]))
+
+    def var(self, items):
+        return ('var', str(items[0]))
+
+    def assign(self, items):
+        return ('assign', str(items[0]), items[1])
+
+    def print(self, items):
+        return ('print', items[0])
+
+    def add(self, items):
+        return ('add', items[0], items[1])
+
+    def sub(self, items):
+        return ('sub', items[0], items[1])
+
+    def mul(self, items):
+        return ('mul', items[0], items[1])
+
+    def div(self, items):
+        return ('div', items[0], items[1])
